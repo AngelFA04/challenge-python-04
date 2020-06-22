b682ed5a51df4b48d507ee08312489b342b4993e
Angel Flores <cyberangel1000@gmail.com>
2020-06-22 11:47:39 -0500

    Challenged solver

--- html_decorators.py
+++ html_decorators.py
@@ -1,29 +1,33 @@
 def div(func):
-    # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f"<div>{func(*args, **kwargs)}</div>"
+    return wrapper
+    
 
 
 def article(func):
-    # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f"<article>{func(*args, **kwargs)}</farticle>"
+    return wrapper
 
 
 def p(func):
-    # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f"<p>{func(*args, **kwargs)}</p>"
+    return wrapper
 
 
 # Here you must apply the decorators, uncomment this later
-# @div
-# @article
-# @p
+#@article
+#@p    
+#@div
 def saludo(nombre):
     return f'¡Hola {nombre}, ¿Cómo estás?'
 
 
 def run():
     print(saludo('Jorge'))
-
+    
 
 if __name__ == '__main__':
     run()
